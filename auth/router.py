8dc8abe442db54834c68231bc098297e49894bb3
roman-zahoruiko <roman.zahoruiko@icloud.com>
2024-06-08 12:36:13 +0300

    add routes summary

--- auth/router.py
+++ auth/router.py
@@ -9,14 +9,14 @@ auth_router = APIRouter()
 
 auth_router.include_router(
     fastapi_users.get_auth_router(auth_backend),
-    prefix="/auth/jwt",
-    tags=["Auth"],
+    prefix='/auth/jwt',
+    tags=['Auth'],
 )
 
 auth_router.include_router(
     fastapi_users.get_register_router(UserReadSchema, UserCreateSchema),
-    prefix="/auth",
-    tags=["Auth"],
+    prefix='/auth',
+    tags=['Auth'],
 )
 
 
@@ -26,18 +26,18 @@ admin_router = APIRouter(
 )
 
 
-@admin_router.put("/users/role")
+@admin_router.put('/users/role', summary='Update user role')
 async def update_user_role(role_update: RoleUpdateSchema, current_user: User = Depends(admin_user)) -> UserReadSchema:
 
     updated_user = await UserRepository.update_user_role(role_update=role_update)
 
     if not updated_user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='User not found')
 
     return UserReadSchema.from_orm(updated_user)
 
 
-@admin_router.post('/users/assign_user')
+@admin_router.post('/users/assign_user', summary='Assign user to manager')
 async def assign_users_to_manager(
     assignment: ManagerUserAssignmentSchema,
     current_user: User = Depends(admin_user),
--- message/router.py
+++ message/router.py
@@ -11,25 +11,25 @@ message_router = APIRouter(
 )
 
 
-@message_router.post('/add')
+@message_router.post('/add', summary='Add new message, send it to TG channel',)
 async def add_message(msg: MessageCreateSchema, user: User = Depends(user_access)) -> MessageResponseSchema:
     message_obj = await MessageRepository.add_one(msg=msg, user=user)
     return MessageResponseSchema.from_orm(message_obj)
 
 
-@message_router.get('/my')
+@message_router.get('/my', summary='Get current user messages')
 async def get_user_messages(user: User = Depends(user_access)) -> List[MessageResponseSchema]:
     messages = await MessageRepository.get_user_messages(user=user)
     return [MessageResponseSchema.from_orm(message) for message in messages]
 
 
-@message_router.get('/manager')
+@message_router.get('/manager', summary='Get assigned users messages')
 async def get_manager_messages(user: User = Depends(manager_user)) -> List[MessageResponseSchema]:
     messages = await MessageRepository.get_manager_messages(user=user)
     return [MessageResponseSchema.from_orm(message) for message in messages]
 
 
-@message_router.get('/admin')
+@message_router.get('/admin', summary='Get all messages (admin only)')
 async def get_admin_messages(user: User = Depends(admin_user)) -> List[MessageResponseSchema]:
     messages = await MessageRepository.get_admin_messages()
     return [MessageResponseSchema.from_orm(message) for message in messages]
