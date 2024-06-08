1c14de5cd653edd904dd61533599602650c66805
roman-zahoruiko <roman.zahoruiko@icloud.com>
2024-06-08 14:11:18 +0300

    add Dockerfile, docker-compose.yml, .env.sample

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-from dotenv import load_dotenv
 import os
 
-load_dotenv()
+# from dotenv import load_dotenv
+# load_dotenv()
 
 DB_HOST = os.environ.get('DB_HOST')
 DB_PORT = os.environ.get('DB_PORT')
